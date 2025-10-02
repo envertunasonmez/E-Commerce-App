ddf2180e8f0a19f7ca7ef6e289b1a796fe8e6b00
Enver Tuna Sönmez <etsonmez01@gmail.com>
2025-10-02 16:11:12 +0300

    update - main view

--- lib/view/main_view.dart
+++ lib/view/main_view.dart
@@ -2,19 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:basic_e_commerce_app/cubit/category/category_cubit.dart';
+import 'package:basic_e_commerce_app/cubit/navigation/navigation_cubit.dart';
 import 'package:basic_e_commerce_app/view/cart_view.dart';
 import 'package:basic_e_commerce_app/view/favorites_view.dart';
 import 'package:basic_e_commerce_app/view/products_view.dart';
 
-class MainView extends StatefulWidget {
+class MainView extends StatelessWidget {
   const MainView({super.key});
-
-  @override
-  State<MainView> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainView> {
-  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +18,6 @@ class _MainPageState extends State<MainView> {
           if (categories.isEmpty) {
             return const Center(child: CircularProgressIndicator());
           }
-
           return ListView.builder(
             itemCount: categories.length,
             itemBuilder: (context, i) {
@@ -47,18 +40,28 @@ class _MainPageState extends State<MainView> {
       const CartView(),
     ];
 
-    return Scaffold(
-      appBar: AppBar(title: const Text("Mini E-Commerce")),
-      body: pages[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (i) => setState(() => _currentIndex = i),
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: "Favorites"),
-          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: "Cart"),
-        ],
-      ),
+    return BlocBuilder<NavigationCubit, int>(
+      builder: (context, currentIndex) {
+        return Scaffold(
+          appBar: AppBar(title: const Text("Mini E-Commerce")),
+          body: pages[currentIndex],
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: currentIndex,
+            onTap: (i) => context.read<NavigationCubit>().setIndex(i),
+            items: const [
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.favorite),
+                label: "Favorites",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_cart),
+                label: "Cart",
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }

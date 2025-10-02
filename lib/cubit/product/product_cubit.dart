a1f5bd6db085dbf6f963d2267c3b13a9f63f4201
Enver Tuna Sönmez <etsonmez01@gmail.com>
2025-10-02 16:05:39 +0300

    add - state

--- lib/cubit/product/product_cubit.dart
+++ lib/cubit/product/product_cubit.dart
@@ -1,14 +1,15 @@
+import 'package:basic_e_commerce_app/cubit/product/product_state.dart';
 import 'package:basic_e_commerce_app/data/models/product_model.dart';
 import 'package:basic_e_commerce_app/data/services/api_service.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class ProductCubit extends Cubit<List<Product>> {
+class ProductCubit extends Cubit<ProductState?> {
   final ApiService api;
-  ProductCubit(this.api) : super([]);
+  ProductCubit(this.api) : super(null);
 
   Future<void> loadProducts(String category) async {
     final data = await api.fetchProductsByCategory(category);
     final products = (data as List).map((e) => Product.fromJson(e)).toList();
-    emit(products);
+    emit(ProductState(category: category, products: products));
   }
 }

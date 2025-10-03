ba9157c0618a0304ee2c8bf9caa4ac3254d841c0
Enver Tuna Sönmez <etsonmez01@gmail.com>
2025-10-03 23:45:06 +0300

    update - custom text form field

--- lib/product/widgets/custom_text_form_field.dart
+++ lib/product/widgets/custom_text_form_field.dart
@@ -41,23 +41,45 @@ class CustomTextFormField extends StatelessWidget {
           ? TextInputType.emailAddress
           : TextInputType.text,
       textInputAction: TextInputAction.next,
+      style: TextStyle(fontSize: 15, color: Colors.grey.shade800),
       decoration: InputDecoration(
         labelText: label,
+        labelStyle: TextStyle(color: Colors.grey.shade600, fontSize: 15),
         filled: true,
-        fillColor: Colors.grey[100],
+        fillColor: Colors.grey.shade50,
         contentPadding: const EdgeInsets.symmetric(
-          horizontal: 16,
-          vertical: 14,
+          horizontal: 20,
+          vertical: 18,
         ),
         border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12),
-          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.circular(14),
+          borderSide: BorderSide(color: Colors.grey.shade200, width: 1.5),
         ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(14),
+          borderSide: BorderSide(color: Colors.grey.shade200, width: 1.5),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(14),
+          borderSide: BorderSide(color: Colors.blue.shade400, width: 2),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(14),
+          borderSide: BorderSide(color: Colors.red.shade300, width: 1.5),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(14),
+          borderSide: BorderSide(color: Colors.red.shade400, width: 2),
+        ),
+        errorStyle: TextStyle(color: Colors.red.shade600, fontSize: 12),
         suffixIcon: isPassword
             ? IconButton(
                 icon: Icon(
-                  obscureText ? Icons.visibility_off : Icons.visibility,
-                  color: Colors.grey[600],
+                  obscureText
+                      ? Icons.visibility_off_outlined
+                      : Icons.visibility_outlined,
+                  color: Colors.grey.shade500,
+                  size: 22,
                 ),
                 onPressed: () {
                   context.read<PasswordVisibilityCubit>().toggle();

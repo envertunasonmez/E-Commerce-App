bea14bd0a71647377f4695b23b22c02242e08004
Enver Tuna Sönmez <etsonmez01@gmail.com>
2025-10-03 15:52:46 +0300

    create - register state

---
+++ lib/cubit/auth/register/register_state.dart
@@ -0,0 +1,23 @@
+class RegisterState {
+  final bool isLoading;
+  final bool isSuccess;
+  final String? errorMessage;
+
+  const RegisterState({
+    this.isLoading = false,
+    this.isSuccess = false,
+    this.errorMessage,
+  });
+
+  RegisterState copyWith({
+    bool? isLoading,
+    bool? isSuccess,
+    String? errorMessage,
+  }) {
+    return RegisterState(
+      isLoading: isLoading ?? this.isLoading,
+      isSuccess: isSuccess ?? this.isSuccess,
+      errorMessage: errorMessage,
+    );
+  }
+}

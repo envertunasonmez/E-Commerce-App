629698d765d32d23595a46094788f40ba34baf20
Enver Tuna Sönmez <etsonmez01@gmail.com>
2025-10-06 02:23:51 +0300

    add - equatable

--- lib/cubit/product/product_state.dart
+++ lib/cubit/product/product_state.dart
@@ -1,8 +1,12 @@
+import 'package:equatable/equatable.dart';
 import 'package:basic_e_commerce_app/data/models/product_model.dart';
 
-class ProductState {
+class ProductState extends Equatable {
   final String category;
   final List<Product> products;
 
-  ProductState({required this.category, required this.products});
+  const ProductState({required this.category, required this.products});
+
+  @override
+  List<Object?> get props => [category, products];
 }

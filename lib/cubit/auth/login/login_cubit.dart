10e6b9f7c85db338e0ba38161020b7dd34a721b8
Enver Tuna Sönmez <etsonmez01@gmail.com>
2025-10-06 02:14:31 +0300

    fix - login cubit

--- lib/cubit/auth/login/login_cubit.dart
+++ lib/cubit/auth/login/login_cubit.dart
@@ -3,15 +3,16 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
-  LoginCubit() : super(const LoginState());
+  final FirebaseAuth _auth;
+
+  LoginCubit({FirebaseAuth? auth})
+    : _auth = auth ?? FirebaseAuth.instance,
+      super(const LoginState());
 
   Future<void> login(String email, String password) async {
     emit(state.copyWith(isLoading: true, errorMessage: null));
     try {
-      await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
       emit(state.copyWith(isLoading: false, isSuccess: true));
     } on FirebaseAuthException catch (e) {
       emit(state.copyWith(isLoading: false, errorMessage: e.message));

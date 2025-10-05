d0c03582f7051bc5cb9140cd39e959fae6ec4b6c
Enver Tuna Sönmez <etsonmez01@gmail.com>
2025-10-06 01:04:16 +0300

    fix - import

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:basic_e_commerce_app/cubit/navigation/navigation_cubit.dart';
 import 'package:basic_e_commerce_app/firebase_options.dart';
 import 'package:basic_e_commerce_app/view/auth/login/login_view.dart';
-import 'package:basic_e_commerce_app/view/main_view.dart';
+import 'package:basic_e_commerce_app/view/main/main_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
